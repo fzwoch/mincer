fd7dc86d956da4cb5313fdcb13872f97900b2456
Florian Zwoch <fzwoch@gmail.com>
2015-01-27 18:10:58 +0100

    revert last commit. i got confused..

--- deploy_bundle.sh
+++ deploy_bundle.sh
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 2 ]; then
 	echo "usage: $0 <Bundle.app> <Bundle.app/Contents/MacOS/application> [..]"
 	exit 1
 fi
